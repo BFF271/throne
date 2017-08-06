506a37fe29fb25475c052282011fd43e6b814751
tobiaswang271 <tobias.n.wang@gmail.com>
2017-08-06 01:51:09 +0100

    Added localstorage to hold redux state

--- src/store.js
+++ src/store.js
@@ -3,4 +3,16 @@ import { createStore } from 'redux';
 // import the combined reducers from ./reducers/index.js
 import reducer from './reducers';
 
-export default createStore(reducer);
+// Get existing state from localStorage
+const persistedState = localStorage.getItem('reduxSocialAppState') ? JSON.parse(localStorage.getItem('reduxSocialAppState')) : {}
+
+// Create Store with data
+const store = createStore(reducer, persistedState);
+
+// Listen for any changes to the state and update localStorage
+store.subscribe(()=>{
+  localStorage.setItem('reduxSocialAppState', JSON.stringify(store.getState()))
+  console.log(localStorage.getItem('reduxSocialAppState'));
+})
+
+export default store;
