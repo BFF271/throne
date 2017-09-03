f04578f5f2d459719d19b84b9a44d0119d2068b7
tobiaswang271 <tobias.n.wang@gmail.com>
2017-09-03 01:10:23 +0100

    Added search functionality to user list

--- src/containers/List.js
+++ src/containers/List.js
@@ -3,9 +3,40 @@ import React, { Component } from 'react';
 import ListItem from './../components/ListItem';
 
 class List extends Component {
+  constructor(props){
+    super(props);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.state = {
+      searchTerm: "",
+      filteredList: this.props.list
+    }
+  }
+
+  handleInputChange(e) {
+    let updatedList = this.props.list;
+
+    updatedList = updatedList.filter(function(item){
+      return item.fullname.toLowerCase().includes(e.target.value.toLowerCase()) || item.username.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    // Update the input changes
+    this.setState({
+      filteredList: updatedList,
+      searchTerm: e.target.value
+    }, console.log(this.state));
+  }
+  //
+  // filterList: function(event){
+  //   var updatedList = this.state.initialItems;
+  //   updatedList = updatedList.filter(function(item){
+  //     return item.toLowerCase().search(
+  //       event.target.value.toLowerCase()) !== -1;
+  //   });
+  //   this.setState({items: updatedList});
+  // },
+
   render() {
 
-    const userList = this.props.list.map((user) => {
+    const userList = this.state.filteredList.map((user) => {
       return (
         <ListItem
           key={user.id}
@@ -17,7 +48,21 @@ class List extends Component {
 
     return (
       <div>
-        <h4>User List</h4>
+        <div className="row">
+          <div className="col-md-6">
+            <h4>User List</h4>
+          </div>
+          <div className="col-md-6">
+            <form className="form-inline">
+              <label>Search For User:</label>
+              <input
+                value={this.state.searchTerm}
+                onChange={this.handleInputChange}
+                className="form-control"
+                type="text"/>
+            </form>
+          </div>
+        </div>
         <hr />
         <div>
           {userList}
